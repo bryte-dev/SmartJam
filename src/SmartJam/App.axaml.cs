7de9a4e4a5ffc4655e3a88e2e4d6f50aabb9c20f
bryte-dev <143592308+bryte-dev@users.noreply.github.com>
2026-05-05 12:03:53 +0200

    fixed drag on oscillator box and added a reset button for played notes

--- src/SmartJam/App.axaml.cs
+++ src/SmartJam/App.axaml.cs
@@ -1,10 +1,6 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
-using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
-using SmartJam.ViewModels;
 using SmartJam.Views;
 
 namespace SmartJam;
@@ -20,10 +16,7 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainWindowViewModel(),
-            };
+            desktop.MainWindow = new MainWindow();
         }
 
         base.OnFrameworkInitializationCompleted();
